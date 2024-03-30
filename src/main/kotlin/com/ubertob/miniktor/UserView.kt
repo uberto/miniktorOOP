8ad55711b5cee311323117b2ca5361ead6ccf963
uberto <uberto.gama@gmail.com>
2024-03-30 21:29:16 +0900

    restored correct service and view

--- src/main/kotlin/com/ubertob/miniktor/UserView.kt
+++ src/main/kotlin/com/ubertob/miniktor/UserView.kt
@@ -3,74 +3,77 @@ package com.ubertob.miniktor
 import kotlinx.html.*
 
 
-fun indexHtml(): HTML.() -> Unit = {
-    head {
-        title("Welcome to MiniKtor")
-    }
-    body {
-        h1 {
-            +"Pages"
+class UserView {
+    fun indexHtml(): HTML.() -> Unit = {
+        head {
+            title("Welcome to MiniKtor")
         }
-        div {
-            a("users") { +"User List" }
+        body {
+            h1 {
+                +"Pages"
+            }
+            div {
+                a("users") { +"User List" }
+            }
         }
     }
-}
 
 
-fun usersPage(users: List<User>): HTML.() -> Unit = {
-    head {
-        title("Users")
-        link(rel = "stylesheet", href = "/static/styles.css", type = "text/css") // Assuming you have some CSS
-    }
-    body {
-        h1 { +"User List" }
-        div(classes = "user-list") {
-            ul {
-                for (user in users) {
-                    li {
-                        +"${user.name} born on ${user.dateOfBirth}"
+    fun usersPage(users: List<User>): HTML.() -> Unit = {
+        head {
+            title("Users")
+            link(rel = "stylesheet", href = "/static/styles.css", type = "text/css") // Assuming you have some CSS
+        }
+        body {
+            h1 { +"User List" }
+            div(classes = "user-list") {
+                ul {
+                    for (user in users) {
+                        li {
+                            +"${user.id} - ${user.name} born on ${user.dateOfBirth}"
+                        }
                     }
                 }
             }
+            // Other HTML content and scripts can be added here
         }
-        // Other HTML content and scripts can be added here
     }
-}
 
-fun userPage(user: User): HTML.() -> Unit = {
-    head {
-        title("User Details")
-        link(rel = "stylesheet", href = "/static/styles.css", type = "text/css")
-    }
-    body {
-        h1 { +"User Details" }
-        div(classes = "user-details") {
-            h2 { +"ID: ${user.id}" }
-            p { +"Name: ${user.name}" }
-            p { +"Date of Birth: ${user.dateOfBirth}" }
+    fun userPage(user: User): HTML.() -> Unit = {
+        head {
+            title("User Details")
+            link(rel = "stylesheet", href = "/static/styles.css", type = "text/css")
         }
-        // You might have a link to go back to the user list
-        a(href = "/users") {
-            +"Back to user list"
+        body {
+            h1 { +"User Details" }
+            div(classes = "user-details") {
+                h2 { +"ID: ${user.id}" }
+                p { +"Name: ${user.name}" }
+                p { +"Date of Birth: ${user.dateOfBirth}" }
+            }
+            // You might have a link to go back to the user list
+            a(href = "/users") {
+                +"Back to user list"
+            }
         }
     }
-}
 
-fun errorPage(errorMessage: String): HTML.() -> Unit = {
-    head {
-        title("Error")
-    }
-    body {
-        h1 { +"Error" }
-        p {
-            +"An error occurred: $errorMessage"
+    fun errorPage(errorMessage: String): HTML.() -> Unit = {
+        head {
+            title("Error")
         }
-        // Link to go back to the user list or home
-        a(href = "/users") {
-            +"Back to user list"
+        body {
+            h1 { +"Error" }
+            p {
+                +"An error occurred: $errorMessage"
+            }
+            // Link to go back to the user list or home
+            a(href = "/users") {
+                +"Back to user list"
+            }
         }
     }
+
 }
 
 
